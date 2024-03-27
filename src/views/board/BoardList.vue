2718e9924d4aa44229eaeff07af2ef2afd68b763
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2024-03-27 23:06:35 +0900

    pagination

--- src/views/board/BoardList.vue
+++ src/views/board/BoardList.vue
@@ -21,22 +21,15 @@
       </tr>
       </tbody>
     </table>
-    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">
+    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.totalPage > 0">
       <span class="pg">
-      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>
-      <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"
-         class="prev w3-button w3-bar-item w3-border">&lt;</a>
-      <template v-for=" (n,index) in paginavigation()">
-          <template v-if="paging.page==n">
-              <strong class="w3-button w3-bar-item w3-border w3-green" :key="index">{{ n }}</strong>
-          </template>
-          <template v-else>
-              <a class="w3-button w3-bar-item w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
-          </template>
-      </template>
-      <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"
-         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-bar-item w3-border">&gt;</a>
-      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>
+        <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>
+        <a href="javascript:;" v-if="paging.prev" @click="fnPage(paging.page - 1)" class="prev w3-button w3-bar-item w3-border">&lt;</a>
+        <template v-for="pageNumber in paging.pageList" :key="pageNumber">
+          <a :class="{ 'w3-button': true, 'w3-bar-item': true, 'w3-border': true, 'active': pageNumber === paging.page }" href="javascript:;" @click="fnPage(pageNumber)">{{ pageNumber }}</a>
+        </template>
+        <a href="javascript:;" v-if="paging.next" @click="fnPage(paging.page + 1)" class="next w3-button w3-bar-item w3-border">&gt;</a>
+        <a href="javascript:;" @click="fnPage(paging.totalPage)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>
       </span>
     </div>
   </div>
@@ -46,33 +39,20 @@
 export default {
   data() { //변수생성
     return {
-      requestBody: {}, //리스트 페이지 데이터전송
-      list: {}, //리스트 데이터
-      no: '', //게시판 숫자처리
+      requestBody: {
+        page: 1,
+        size: 10 // 페이지당 아이템 수
+      },
+      list: [], // 리스트 데이터
       paging: {
-        block: 0,
-        end_page: 0,
-        next_block: 0,
-        page: 0,
-        page_size: 0,
-        prev_block: 0,
-        start_index: 0,
-        start_page: 0,
-        total_block_cnt: 0,
-        total_list_cnt: 0,
-        total_page_cnt: 0,
-      }, //페이징 데이터
-      page: this.$route.query.page ? this.$route.query.page : 1,
-      size: this.$route.query.size ? this.$route.query.size : 10,
-      keyword: this.$route.query.keyword,
-      paginavigation: function () { //페이징 처리 for문 커스텀
-        let pageNumber = [] //;
-        let start_page = this.paging.start_page;
-        let end_page = this.paging.end_page;
-        for (let i = start_page; i <= end_page; i++) pageNumber.push(i);
-        return pageNumber;
+        totalPage: 0,
+        page: 1,
+        size: 10,
+        prev: false,
+        next: false,
+        pageList: []
       }
-    }
+    };
   },
   mounted() {
     this.fnGetList()
@@ -90,10 +70,15 @@ export default {
         headers: {}
       }).then((res) => {
 
-        console.log(res.data);
-        this.list = res.data.data.dtoList;  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
-
-
+        if (res.data.statusCode === "OK") {
+          this.list = res.data.data.dtoList; //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+          this.paging.totalPage = res.data.data.totalPage;
+          this.paging.page = res.data.data.page;
+          this.paging.size = res.data.data.size;
+          this.paging.prev = res.data.data.prev;
+          this.paging.next = res.data.data.next;
+          this.paging.pageList = res.data.data.pageList;
+        }
       }).catch((err) => {
         if (err.message.indexOf('Network Error') > -1) {
           alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
@@ -121,3 +106,23 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.pagination {
+  margin-top: 20px;
+}
+
+.pagination .pg {
+  display: flex;
+  justify-content: center;
+}
+
+.pagination .pg a {
+  margin: 0 5px;
+}
+
+.pagination .pg a.active {
+  background-color: #ccc;
+  color: #fff;
+}
+</style>
